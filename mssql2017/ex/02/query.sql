02fd371a15b6af7f61699f7a1c5710796e30fde3
lana <sitdownrightnow2552@gmail.com>
2019-12-18 15:51:26 +0700

    add triggers

--- mssql2017/ex/02/query.sql
+++ mssql2017/ex/02/query.sql
@@ -9,23 +9,46 @@ FROM Travels
 GROUP BY Categories.catId, catName
 
 UPDATE Categories
-SET counts = (
-    SELECT COUNT(trId)
-    FROM Travels
-    WHERE Travels.catId = Categories.catId
-)
+SET counts = (SELECT COUNT(trId)
+              FROM Travels
+              WHERE Travels.catId = Categories.catId)
+GO
 
-CREATE TRIGGER TRG_UPD_Travels
+DROP TRIGGER IF EXISTS TRG_Price_Travels
+GO
+CREATE TRIGGER TRG_Price_Travels
     ON Travels
-    AFTER UPDATE
+    FOR UPDATE, INSERT
     AS
-    IF ((SELECT COUNT(price)
-         FROM inserted
-         WHERE price < 0) > 0)
+    IF EXISTS(SELECT *
+              FROM inserted
+              WHERE price < 0)
         BEGIN
-            DELETE Travels
-            FROM Travels
-                     RIGHT JOIN inserted ON Travels.trId = inserted.trId;
             THROW 51000, 'Travel price must > 0',1
         END
+GO
 
+DROP TRIGGER IF EXISTS TRG_CntUpd_Categories
+GO
+CREATE TRIGGER TRG_CntUpd_Categories
+    ON Travels
+    FOR DELETE
+    AS
+    UPDATE Categories
+    SET counts = (SELECT COUNT(trId)
+                  FROM Travels
+                  WHERE Travels.catId = Categories.catId)
+    FROM Categories
+             RIGHT JOIN deleted ON deleted.catId = Categories.catId
+GO
+
+DROP TRIGGER IF EXISTS TRG_Date_Travels
+GO
+CREATE TRIGGER TRG_Date_Travels
+    ON Travels
+    FOR INSERT
+    AS
+    IF EXISTS(SELECT *
+              FROM inserted
+              WHERE startDate < GETDATE())
+        THROW 52000, 'You aint no time traveler',1
