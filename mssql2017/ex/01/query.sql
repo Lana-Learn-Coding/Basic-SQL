6afa904c79bbe54bb9118099b3f570494aa9763f
lana <sitdownrightnow2552@gmail.com>
2019-12-18 10:59:49 +0700

    view and procedure

--- mssql2017/ex/01/query.sql
+++ mssql2017/ex/01/query.sql
@@ -24,3 +24,28 @@ WHERE Status IS NULL
 
 CREATE INDEX EmpName_Idx ON Employee (EmpName)
 
+DROP VIEW IF EXISTS vwEmployee
+CREATE VIEW vwEmployee AS
+SELECT EmpName        AS [Ten Nhan Vien],
+       DepartmentName AS [Ten Phong Ban],
+       Location       AS [Noi Lam Viec],
+       EmpTypeName    AS [Loai Nhan Vien]
+FROM Employee E
+         LEFT JOIN EmployeeType ET ON E.EmpTypeId = ET.EmpTypeId
+         LEFT JOIN Department D ON E.DepartmentId = D.DepartmentId
+GO
+
+CREATE PROCEDURE SelectBySalary @min FLOAT, @max FLOAT
+AS
+SELECT *
+FROM Employee
+WHERE Salary >= @min
+  AND Salary <= @max
+GO
+
+CREATE PROCEDURE SelectBySalaryBiggerThan7Mil
+AS
+SELECT *
+FROM Employee
+WHERE Salary >= 7000000
+GO
